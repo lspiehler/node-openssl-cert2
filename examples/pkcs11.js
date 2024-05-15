b6461b109ef4a136ab16982f75bb98def6e675e7
Lyas Spiehler <lspiehler@gmail.com>
2024-05-15 11:31:16 +0000

    working on pkcs11 lib

--- examples/pkcs11.js
+++ examples/pkcs11.js
@@ -1,20 +1,36 @@
 const node_openssl = require('../index.js');
-var openssl = new node_openssl({binpath: 'openssl', debug: true});
+var openssl = new node_openssl({binpath: 'openssl', debug: false});
 
 openssl.pkcs11.listSlots({modulePath: openssl.binary.pkcs11Tool.getLibDir() + '/x86_64-linux-gnu/libykcs11.so'}, function(err, slots, cmd) {
     if(err) {
         console.log(err);
     } else {
         console.log(slots);
-        openssl.pkcs11.listObjects({
-            modulePath: openssl.binary.pkcs11Tool.getLibDir() + '/x86_64-linux-gnu/libykcs11.so',
-            slotid: slots[0].hexid
-        }, function(err, objects) {
-            if(err) {
-                console.log(err);
-            } else {
-                console.log(objects);
-            }
-        });
+        if(slots.data.length < 1) {
+            console.log('no slots found');
+        } else {
+            openssl.pkcs11.listObjects({
+                modulePath: openssl.binary.pkcs11Tool.getLibDir() + '/x86_64-linux-gnu/libykcs11.so',
+                slotid: slots.data[0].hexid
+            }, function(err, objects) {
+                if(err) {
+                    console.log(err);
+                } else {
+                    console.log(objects);
+                    openssl.pkcs11.readObject({
+                        modulePath: openssl.binary.pkcs11Tool.getLibDir() + '/x86_64-linux-gnu/libykcs11.so',
+                        slotid: slots.data[0].hexid,
+                        type: 'cert',
+                        objectid: objects.data[0]['ID']
+                    }, function(err, object) {
+                        if(err) {
+                            console.log(err);
+                        } else {
+                            console.log(object.data);
+                        }
+                    });
+                }
+            });
+        }
     }
 });
