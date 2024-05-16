ad7cf8870490c7125b6ddbfded2f11f8a86577eb
Lyas Spiehler <lspiehler@gmail.com>
2024-05-16 11:25:36 +0000

    revert crl changes and don't write keyfile if a ket isn't specified

--- examples/crl.js
+++ examples/crl.js
@@ -214,32 +214,6 @@ openssl.keypair.generateRSA(rootcarsaoptions, function(err, rootcarsa) {
 																				console.log(err);
 																			} else {
 																				console.log(crl.data);
-																				binary.openssl.getVersion(function(err, version) {
-																					if(err) {
-																						callback('Failed to get openssl version: ' + err, {});
-																					} else {
-																						if(version.substring(0, 1) == "3") {
-																							let revoked = [];
-																							revoked['6C1B17D5E80FF201A0BCB6BF1502F809E3A3FECE'] = 'keyCompromise'
-																							//revoked['6C1B17D5E80FF201A0BCB6BF1502F809E3A3FECE'] = 'superseded'
-																							openssl.crl.generate({
-																								key: subcarsa.data,
-																								password: subcarsaoptions.encryption.password,
-																								ca: subcacert.data,
-																								crldays: 90,
-																								revoked: revoked,
-																								crl: crl.data
-																							}, function(err, crl) {
-																								if(err) {
-																									console.log(err);
-																								} else {
-																									console.log(crl.data);
-																									console.log(crl);
-																								}
-																							});
-																						}
-																					}
-																				});
 																			}
 																		});
 																	}
