590938014f33763dffa79bbeb56696a1dd50b4f1
Lyas Spiehler <lspiehler@gmail.com>
2026-01-07 13:31:06 +0000

    test SLH-DSA keys

--- examples/oqs_key.js
+++ examples/oqs_key.js
@@ -1,10 +1,11 @@
 const node_openssl = require('../index.js');
-var openssl = new node_openssl({binpath: '/opt/openssl32/bin/openssl', debug: false});
+var openssl = new node_openssl({});
 
 //const algorithm = 'sphincsshake128fsimple'
-//const algorithm = 'mldsa44'
+// const algorithm = 'mldsa44'
+const algorithm = 'slh-dsa-sha2-128f'
 //const algorithm = 'p256_sphincssha2128fsimple'
-const algorithm = 'sphincsshake192fsimple'
+// const algorithm = 'sphincsshake192fsimple'
 //const algorithm = 'falcon512'
 
 let options = {
@@ -15,6 +16,6 @@ openssl.keypair.generateOQSKey(options, function(err, key) {
     if(err) {
         console.log(err);
     } else {
-        console.log(key);
+        console.log(key.data);
     }
 });
